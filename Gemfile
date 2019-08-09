fe5b58f390f338585de538d80dd3f4f8657bfff7
Andrey <andrey.korolko@mail.ru>
2019-08-09 10:26:19 +0300

    add tests

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ end
 group :test do
   gem 'capybara', '>= 2.15'
   gem 'chromedriver-helper'
+  gem 'factory_bot', '~> 5.0'
+  gem 'rails-controller-testing', '~> 1.0', '>= 1.0.4'
   gem 'selenium-webdriver'
 end
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,9 +13,15 @@ end
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   config.infer_spec_type_from_file_location!
 
   config.filter_rails_from_backtrace!
+
+  config.include FactoryBot::Syntax::Methods
+
+  config.before(:all) do
+    FactoryBot.reload
+  end
 end
