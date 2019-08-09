f67e333b642374baa0a83ee4c0677d781572d52c
Andrey <andrey.korolko@mail.ru>
2019-08-09 18:40:12 +0300

    add episode creation

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -8,4 +8,28 @@ class EpisodesController < ApplicationController
   def show
     @episode = Episode.find(params[:id])
   end
+
+  def create
+    @episode = Episode.new(episode_params.merge(created_by: current_user))
+
+    if @episode.save
+      redirect_to episodes_url, notice: 'Successfully created'
+    else
+      render :new, notice: 'Something wrong'
+    end
+  end
+
+  private
+
+  def episode_params
+    params[:video] = params[:video].split('=').last
+    params[:date] = datetime
+    params.delete(:time)
+
+    params.permit(:title, :video, :date, :description)
+  end
+
+  def datetime
+    Time.parse("#{params[:date]} #{params[:time]}").utc
+  end
 end
