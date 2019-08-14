bb186134d9acf7b2d7df7870ae5d6c12a865ed48
lokkirill <lokkirill@gmail.com>
2019-08-14 10:48:15 +0300

    Convert views to .slim, add some style on views, rework model specs, add possibility to delete announcements

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,5 +1,6 @@
 class Announcement < ApplicationRecord
   validates :title, :date, :target_resource, presence: true
+  validates :target_resource, inclusion: { in: %w[telegram twitter], message: '%{value} is not a valid resourse' }
 
   belongs_to :episode
 end
--- spec/models/announcement_spec.rb
+++ spec/models/announcement_spec.rb
@@ -2,42 +2,41 @@ require 'rails_helper'
 
 # rubocop:disable Metrics/BlockLength
 RSpec.describe Announcement, type: :model do
-  subject { described_class.new }
+  ANNOUNCEMENT_VALID_PARAMS = { episode_id: 1, title: 'Title', date: Time.now, target_resource: 'telegram' }.freeze
+  ANNOUNCEMENT_INVALID_PARAMS_NO_EPISODE = { title: 'Title', date: Time.now, target_resource: 'telegram' }.freeze
+  ANNOUNCEMENT_INVALID_PARAMS_NO_TITLE = { episode_id: 1, date: Time.now, target_resource: 'telegram' }.freeze
+  ANNOUNCEMENT_INVALID_PARAMS_NO_DATE = { episode_id: 1, title: 'Title', target_resource: 'telegram' }.freeze
+  ANNOUNCEMENT_INVALID_PARAMS_NO_RESOURCE = { episode_id: 1, title: 'Title', date: Time.now }.freeze
+  TELEGRAM_ANNOUNCEMENT_PARAMS = { episode_id: 1, title: 'Title', date: Time.now, target_resource: 'telegram' }.freeze
+  TWITTER_ANNOUNCEMENT_PARAMS = { episode_id: 1, title: 'Title', date: Time.now, target_resource: 'twitter' }.freeze
+  VK_ANNOUNCEMENT_PARAMS = { episode_id: 1, title: 'Title', date: Time.now, target_resource: 'vk' }.freeze
 
-  it 'is valid with valid attributes' do
-    subject.episode_id = 1
-    subject.title = '1st episode announcement title'
-    subject.date = Time.now
-    subject.target_resource = 'tg'
-    expect(subject).to be_valid
+  context 'with valid params' do
+    let(:valid_announcement) { described_class.new(ANNOUNCEMENT_VALID_PARAMS) }
+
+    it { expect(valid_announcement).to be_valid }
   end
 
-  it 'is not valid without episode_id' do
-    subject.title = '1st episode announcement title'
-    subject.date = Time.now
-    subject.target_resource = 'tg'
-    expect(subject).to_not be_valid
+  context 'with invalid params' do
+    let(:announcement_without_episode) { described_class.new(ANNOUNCEMENT_INVALID_PARAMS_NO_EPISODE) }
+    let(:announcement_without_title) { described_class.new(ANNOUNCEMENT_INVALID_PARAMS_NO_TITLE) }
+    let(:announcement_without_date) { described_class.new(ANNOUNCEMENT_INVALID_PARAMS_NO_DATE) }
+    let(:announcement_without_resource) { described_class.new(ANNOUNCEMENT_INVALID_PARAMS_NO_RESOURCE) }
+
+    it { expect(announcement_without_episode).to be_invalid }
+    it { expect(announcement_without_title).to be_invalid }
+    it { expect(announcement_without_date).to be_invalid }
+    it { expect(announcement_without_resource).to be_invalid }
   end
 
-  it 'is not valid without title' do
-    subject.episode_id = 1
-    subject.date = Time.now
-    subject.target_resource = 'tg'
-    expect(subject).to_not be_valid
-  end
+  context 'with available and unavailable resources' do
+    let(:telegram_announcement) { described_class.new(TELEGRAM_ANNOUNCEMENT_PARAMS) }
+    let(:twitter_announcement) { described_class.new(TWITTER_ANNOUNCEMENT_PARAMS) }
+    let(:vk_announcement) { described_class.new(VK_ANNOUNCEMENT_PARAMS) }
 
-  it 'is not valid without date' do
-    subject.episode_id = 1
-    subject.title = '1st episode announcement title'
-    subject.target_resource = 'tg'
-    expect(subject).to_not be_valid
-  end
-
-  it 'is not valid without target_resource' do
-    subject.episode_id = 1
-    subject.title = '1st episode announcement title'
-    subject.date = Time.now
-    expect(subject).to_not be_valid
+    it { expect(telegram_announcement).to be_valid }
+    it { expect(twitter_announcement).to be_valid }
+    it { expect(vk_announcement).to be_invalid }
   end
 end
 # rubocop:enable Metrics/BlockLength
