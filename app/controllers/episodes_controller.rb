bd2b0624888b50795471caff1db43e989195a1f6
Andrey <andrey.korolko@mail.ru>
2019-08-16 13:57:28 +0300

    refactor new, edit view for episode; refactor params processing

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -11,10 +11,9 @@ class EpisodesController < ApplicationController
   def edit; end
 
   def create
-    @episode = Episode.new(params_for_episode)
-    @episode.created_by = current_user
+    @episode = Episode.create(episode_params.merge(created_by: current_user))
 
-    if @episode.save
+    if @episode.valid?
       redirect_to episodes_url, notice: 'Successfully created'
     else
       render :new, notice: 'Something wrong'
@@ -22,7 +21,7 @@ class EpisodesController < ApplicationController
   end
 
   def update
-    if @episode.update(params_for_episode)
+    if @episode.update(episode_params)
       redirect_to episode_path
     else
       redirect_to edit_episode_path
@@ -35,18 +34,7 @@ class EpisodesController < ApplicationController
     @episode = Episode.find(params[:id])
   end
 
-  def permitted_params
+  def episode_params
     params.permit(:title, :video, :date, :description)
-  end
-
-  def params_for_episode(permit_params = permitted_params)
-    permit_params[:video] = permit_params[:video]
-                            .match(%r{(youtu\.be\/|youtube\.com\/(watch\?(.*&)?v=|(embed|v)\/))([^\?&"'>]+)})[5]
-    permit_params[:date] = datetime
-    permit_params
-  end
-
-  def datetime
-    Time.parse("#{params[:date]} #{params[:time]}").utc
   end
 end
--- app/models/episode.rb
+++ app/models/episode.rb
@@ -3,4 +3,24 @@ class Episode < ApplicationRecord
 
   has_many :timecodes
   has_and_belongs_to_many :authors
+
+  VALID_YOUTUBE_LINK = %r{(http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?)}
+  YOUTUBE_VIDEO_IDENTIFIER = %r{(youtu\.be\/|youtube\.com\/(watch\?(.*&)?v=|(embed|v)\/))([^\?&"'>]+)}
+
+  def self.create(args)
+    super(prepare_params(args).to_h)
+  end
+
+  def update(args)
+    super(self.class.prepare_params(args).to_h)
+  end
+
+  private
+
+  def self.prepare_params(args)
+    raise ActiveRecord::RecordInvalid.new unless args[:video].match VALID_YOUTUBE_LINK
+    args[:video] = args[:video].match(YOUTUBE_VIDEO_IDENTIFIER)[5]
+    args[:date] = Time.parse(args[:date]).utc
+    args
+  end
 end
