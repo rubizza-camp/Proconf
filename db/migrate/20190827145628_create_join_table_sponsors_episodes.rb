6c67dda1ca1a2f5c25a134ca67a7db4af31ffc18
Nikita <nikitamasalkov.tang@gmail.com>
2019-08-27 22:42:53 +0300

    refactored migration for sponsors join table

--- db/migrate/20190827145628_create_join_table_sponsors_episodes.rb
+++ db/migrate/20190827145628_create_join_table_sponsors_episodes.rb
@@ -1,8 +1,5 @@
 class CreateJoinTableSponsorsEpisodes < ActiveRecord::Migration[5.2]
   def change
-    create_join_table :sponsors, :episodes do |t|
-      # t.index [:sponsor_id, :episode_id]
-      # t.index [:episode_id, :sponsor_id]
-    end
+    create_join_table :sponsors, :episodes
   end
 end
