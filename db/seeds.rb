a524005637c104631a819be7e59bbdabeba2c442
Halavin Artsiom <halavin.artsiom@gmail.com>
2019-08-27 16:23:37 +0300

    Changed youtube links in episodes seeds, added timecodes seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,15 @@ CSV.foreach(File.realpath('db/data/episodes.csv')) do |row|
   Episode.find_or_create_by(title: row[TITLE],
                             date: row[DATE],
                             video: row[VIDEO],
-                            description: row[DESCRIPTION])
+                            description: row[DESCRIPTION],
+                            broadcast_begin: Time.parse('12-12-2019 13:05').utc)
+end
+
+Episode.all.each do |episode|
+  CSV.foreach(File.realpath('db/data/timecodes.csv')) do |row|
+    episode.timecodes.find_or_create_by(title: row[TITLE],
+                                        time: Time.parse(row[DATE]).utc)
+  end
 end
 
 Role.create(name: 'admin')
