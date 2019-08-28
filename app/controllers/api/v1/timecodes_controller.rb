282a3f048d450ed9ba025c009a0c4db611b75d1c
Halavin Artsiom <halavin.artsiom@gmail.com>
2019-08-28 10:25:47 +0300

    mend

--- app/controllers/api/v1/timecodes_controller.rb
+++ app/controllers/api/v1/timecodes_controller.rb
@@ -1,57 +1,57 @@
 module Api
   module V1
     class TimecodesController < ApiController
-        before_action :set_timecode, only: %i[edit update show destroy send_timecode]
-        before_action :set_episode, only: %i[index new create]
+      before_action :set_timecode, only: %i[edit update show destroy send_timecode]
+      before_action :set_episode, only: %i[index new create]
 
-        def index
-          render json: @episode.timecodes
-        end
-      
-        def show
-          render json: @timecode
-        end
-      
-        def create
-          timecode = @episode.timecodes.create(timecode_params)
+      def index
+        render json: @episode.timecodes
+      end
 
-          if timecode.valid?
-            render json: 'Timecode successfully created!'
-          else
-            render json: 'Error while creation'
-          end
-        end
+      def show
+        render json: @timecode
+      end
 
-        def update
-          if @timecode.update(timecode_params)
-            render json: 'Timecode successfully updated!'
-          else
-            render json: 'Error while update'
-          end
+      def create
+        timecode = @episode.timecodes.create(timecode_params)
+
+        if timecode.valid?
+          render json: 'Timecode successfully created!'
+        else
+          render json: 'Error while creation'
         end
-      
-        def destroy
-          @timecode.delete
-          render json: 'Succesfully'
+      end
+
+      def update
+        if @timecode.update(timecode_params)
+          render json: 'Timecode successfully updated!'
+        else
+          render json: 'Error while update'
         end
-      
-        private
-      
-        def timecode_param_names
-          %i[title time]
-        end
-      
-        def timecode_params
-          params.permit(timecode_param_names)
-        end
-      
-        def set_timecode
-          @timecode = Timecode.find(params[:id])
-        end
-      
-        def set_episode
-          @episode = Episode.find(params['episode_id'])
-        end
-      end 
+      end
+
+      def destroy
+        @timecode.delete
+        render json: 'Succesfully'
+      end
+
+      private
+
+      def timecode_param_names
+        %i[title time]
+      end
+
+      def timecode_params
+        params.permit(timecode_param_names)
+      end
+
+      def set_timecode
+        @timecode = Timecode.find(params[:id])
+      end
+
+      def set_episode
+        @episode = Episode.find(params['episode_id'])
+      end
+    end
   end
 end
