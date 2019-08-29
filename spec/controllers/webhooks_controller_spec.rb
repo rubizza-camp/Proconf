3f889a4a4ced30e1fa099e5d8550b1731e38108f
Halavin Artsiom <halavin.artsiom@gmail.com>
2019-08-29 09:53:02 +0300

    Make test work

--- spec/controllers/webhooks_controller_spec.rb
+++ spec/controllers/webhooks_controller_spec.rb
@@ -14,16 +14,17 @@ RSpec.describe WebhooksController, type: :controller do
 	  			'date': '2013-07-31T16:58:51.949Z'
 		  	}
 	    }
-	  end
+    end
 
 	  it 'returns 200 header to trello' do
 	    get :complete
       expect(response).to have_http_status(200)
     end
 
-		it 'filters webhook request and save timecode' do
+    it 'filters webhook request and save timecode to episode which is online' do
+      Episode.last.update(status: 'online')
       post :receive, body: trello_webhook.to_json
-      expect(Timecode.last.title).to eq(trello_webhook[:action][:data][:card][:name])
+      expect(Episode.find_by(status: 'online').timecodes.last.title).to eq(trello_webhook[:action][:data][:card][:name])
     end
   end
 end
