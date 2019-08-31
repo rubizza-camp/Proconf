581b450b976de6f0411dffbddbc045ab85d6bcbb
Nikita <nikitamasalkov.tang@gmail.com>
2019-08-31 13:17:57 +0300

    added components for guests, authors and sponsors

--- app/javascript/components/episodes/NewEpisodeFormComponent.js
+++ app/javascript/components/episodes/NewEpisodeFormComponent.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import axios from 'axios';
-import { Form, Input, DatePicker, Button, message } from 'antd';
+import { Form, Input, DatePicker, Button, message, Select } from 'antd';
 import moment from 'moment';
 import { withRouter } from 'react-router-dom';
 
 class NewEpisodeFormComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { title: '', date: '', video: '', description: '' };
+    this.state = { title: '', date: '', video: '', description: '', authors: [], guests: [], sponsors: []};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  };
+
+  componentDidMount () {
+      axios.get(`/api/v1/participants`)
+          .then(response => {
+              console.log(response)
+              this.setState({
+                  authors: response.data.authors,
+                  guests: response.data.guests,
+                  sponsors: response.data.sponsors
+              })
+              console.log(this.state)
+
+          })
+          .catch(function (error) {
+              console.log(error);
+          });
   };
 
   handleChange(event) {
@@ -50,7 +67,7 @@ class NewEpisodeFormComponent extends React.Component {
   };
 
   render() {
-    const { title, video, description } = this.state
+    const { title, video, description, sponsors, guests, authors } = this.state
     return (
       <Form labelCol={{ span: 5 }} wrapperCol={{ span: 12 }} onSubmit={this.handleSubmit}>
         <Form.Item label="Title" required={true}>
@@ -68,6 +85,42 @@ class NewEpisodeFormComponent extends React.Component {
         <Form.Item label="Description">
           {(<Input.TextArea name="description" type="text" value={description} autosize={true} onChange={this.handleChange} />)}
         </Form.Item>
+
+          <Form.Item label="Authors">
+              {<Select
+                  mode="multiple"
+                  style={{ width: '100%' }}
+                  placeholder="Please select"
+              >
+                  {authors.map(function (author) {
+                      return <Option key={author.id}>{author.name}</Option>
+                  })}
+              </Select>}
+          </Form.Item>
+
+          <Form.Item label="Guests">
+              {<Select
+                  mode="multiple"
+                  style={{ width: '100%' }}
+                  placeholder="Please select"
+              >
+                  {guests.map(function (guest) {
+                      return <Option key={guest.id}>{guest.name}</Option>
+                  })}
+              </Select>}
+          </Form.Item>
+
+          <Form.Item label="Sponsors">
+              {<Select
+                  mode="multiple"
+                  style={{ width: '100%' }}
+                  placeholder="Please select"
+              >
+                  {sponsors.map(function (sponsor) {
+                      return <Option key={sponsor.id}>{sponsor.name}</Option>
+                  })}
+              </Select>}
+          </Form.Item>
 
         <Form.Item wrapperCol={{ span: 12, offset: 5 }}>
           <Button type="primary" htmlType="submit">
