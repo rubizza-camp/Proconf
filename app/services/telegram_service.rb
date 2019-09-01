05acab5d2f6fe628cbb0e31abdbcf298f18c20bf
lokkirill <lokkirill@gmail.com>
2019-09-01 15:54:33 +0300

    Add poss to send announcements to telegram

--- app/services/telegram_service.rb
+++ app/services/telegram_service.rb
@@ -5,6 +5,27 @@ class TelegramService
   MESSAGE_PATH = '&text='.freeze
   MESSAGE = "I'm trying to check telegram integration :D".freeze
 
+  # rubocop:disable all
+  class << self
+    def send_announcement(user, announcement)
+      @message = prepare_message(announcement)
+      params = user.credentials.find_by(service: 'telegram')&.data
+      if announcement.image
+        url = "#{TELEGRAM_API_URL}#{params['telegram_token']}#{SEND_PHOTO_PATH}#{params['telegram_chat_id']}#{PHOTO_PATH}#{announcement.image}"
+        JSON.parse(Net::HTTP.get(URI.parse(url)))
+      end
+      url = "#{TELEGRAM_API_URL}#{params['telegram_token']}#{SEND_MESSAGE_PATH}#{params['telegram_chat_id']}#{MESSAGE_PATH}#{@message}"
+      JSON.parse(Net::HTTP.get(URI.parse(URI.encode(url))))
+    end
+
+    private
+
+    def prepare_message(announcement)
+      "Date: #{announcement.date.strftime('%d.%m.%Y %H:%M')}. Title: #{announcement.title} #{announcement.video}"
+    end
+  end
+  # rubocop:enable all
+
   def initialize(user, params)
     @user = user
     @params = params
